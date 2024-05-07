5f3dce8901b067705c3d4187b3e2e605907e66b1
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-06 18:17:50 -0600

    Adding to the store data from the Response of the API, this response contains the Crypto Currency to Display

--- src/store.ts
+++ src/store.ts
@@ -1,22 +1,39 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
-import { Cryptocurrency } from './types';
-import { getCryptos } from './services/CryptoService';
+import { CryptoPrice, Cryptocurrency, Pair } from './types';
+import { getCryptos, fetchCurrentCryptoPrice } from './services/CryptoService';
 
 type CryptoStore = {
     cryptocurrencies: Cryptocurrency[],
-    fetchCryptos: () => Promise<void>;
+    result: CryptoPrice,
+    fetchCryptos: () => Promise<void>,
+    fetchData: (pair: Pair) => Promise<void>;
 };
 
 export const useCryptoStore = create<CryptoStore>()(devtools((set) => ({
 
     cryptocurrencies: [],
+    result: {
+        IMAGEURL: '',
+        PRICE: '',
+        HIGHDAY: '',
+        LOWDAY: '',
+        CHANGEPCT24HOUR: '',
+        LASTUPDATE: ''
+    },
 
     fetchCryptos: async () => {
         const cryptocurrencies = await getCryptos();
 
         set(() => ({
             cryptocurrencies
+        }));
+    },
+    fetchData: async (pair) => {
+        const result = await fetchCurrentCryptoPrice(pair);
+
+        set(() => ({
+            result
         }));
     }
 })));
